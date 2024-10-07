8a6bedcb121cfb53b9ff946d518490542a2465ba
farmcroud <162990102+farmcroud@users.noreply.github.com>
2024-10-07 22:35:47 +0530

    Delete castprojectbackend directory

--- castprojectbackend/src/Routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/uploadRoute.js
-const express = require("express");
-const router = express.Router();
-const { upload, uploadFile } = require("../Controller/UploadController");
-
-// Define the POST route for file uploads
-router.post("/upload", upload.single("file"), uploadFile);
-
-module.exports = router;
